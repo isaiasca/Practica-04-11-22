2058d2fcb4d8c00751b997c874d0009191755436
isaiasca <1307759@senati.pe>
2022-11-04 16:10:59 -0500

    historia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,11 +62,9 @@
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.clima.com/peru/huanuco/tingo-maria" class="underline text-gray-900 dark:text-white">Tingo María</a></div>
-                                
                             </div>
 
                             <div class="ml-12">
-                            <a href="/img/a.jpg"></a>
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                 Tingo María es una ciudad en el río Huallaga, en el centro de Perú, entre las montañas andinas y la selva amazónica. Aquí se encuentra el Parque nacional Tingo María que posee una abundante biodiversidad y es conocido por la Bella Durmiente, una montaña con la forma de una mujer acostada. Además, aquí está la Cueva de las Lechuzas, que alberga guácharos. Algunas de las cascadas locales son Santa Carmen, con una piscina natural, y Gloriapata, a la que se accede a través de un puente suspendido.
                                 </div>
