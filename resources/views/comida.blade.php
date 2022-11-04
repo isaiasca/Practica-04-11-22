f9d533f69f712e362fd8061eb62c6bd4cc65ab2e
isaiasca <1307759@senati.pe>
2022-11-04 18:37:46 -0500

    seguimops con el turismo

--- resources/views/comida.blade.php
+++ resources/views/comida.blade.php
@@ -7,6 +7,36 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>comida juanes</h1>
+    <style>
+         .card{
+            margin-top: 200px;
+            width: 30rem;
+            height: 35rem;
+            margin-left: auto;
+            margin-right: auto;
+            margin: 3px solid black;  
+        }
+        .card-title{
+            margin-top: 10px;
+            text-align: center;
+        }
+
+        .card-text{
+            margin-top: 15px;
+            text-align: center;
+        }
+        .img-card{
+            width: 30rem;
+            height: 20rem;
+        }
+    </style>
+    <div class="card" >
+        <img src="https://buenazo.cronosmedia.glr.pe/original/2021/06/21/60d0a751127e54384e516bb2.jpg" class="img-card" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Juane</h5>
+            <p class="card-text">El juane​ es como un tamal, típico de la gastronomía de la selva peruana y es muy consumido durante la fiesta de San Juan que se celebra el 24 de junio de cada año.</p>
+
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/historia.blade.php
+++ resources/views/historia.blade.php
@@ -8,19 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="img-header">
     <style>
         .card{
-            
-        
             margin-top: 200px;
-           
             width: 30rem;
             height: 35rem;
             margin-left: auto;
-            margin-right: auto;
-            
-            
+            margin-right: auto;   
         }
         .card-title{
             margin-top: 10px;
@@ -34,14 +29,12 @@
 
         .card-link{
             margin-top: 14px;
-            
             margin-left: 150px;
-           
-            
-        }       
+        } 
+         
         
-
     </style>
+    
 
     <div class="card" >
         <img src="https://elperuano.pe/fotografia/thumbnail/2021/10/15/000135611M.jpg" class="img-card" alt="...">
